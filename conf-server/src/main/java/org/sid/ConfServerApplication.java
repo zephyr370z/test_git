ff345e3d166e0827ee4b32b6378fc2accaaf7781
MALYAH <amine.malyah@sogeti.com>
2020-07-06 00:02:32 +0200

    Ajout du Tag EnableConfigServer

--- conf-server/src/main/java/org/sid/ConfServerApplication.java
+++ conf-server/src/main/java/org/sid/ConfServerApplication.java
@@ -2,7 +2,8 @@ package org.sid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
 public class ConfServerApplication {
 
